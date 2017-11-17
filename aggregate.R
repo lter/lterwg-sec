113558978287b7cdae67724335ceb7d32b3c4c01
Celine Mol <celine@link.nceas.ucsb.edu>
2017-11-17 10:35:36 -0800

    changed OR to AND to fix INBOUNDS file

--- aggregate.R
+++ aggregate.R
@@ -104,7 +104,7 @@ write.csv(first_file, output_file, row.names = FALSE, fileEncoding = "UTF-8", qu
 
 # ----- CHECK IF CORRECTLY CONVERTED ----- #
 
-dataset <- read.csv(output_file)
+dataset <- read.csv(bounded_file) #output_file
 str(dataset)
 
 # ----- MEAN ----- #
@@ -149,3 +149,78 @@ for (i in 7:length(names(dataset))) {
 write.csv(means, "MEAN_Summary_Statistics.csv", row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
 write.csv(mins, "MIN_Summary_Statistics.csv", row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
 write.csv(maxs, "MAX_Summary_Statistics.csv", row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
+
+write.csv(means, "INBOUNDS_MEAN_Summary_Statistics.csv", row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
+write.csv(mins, "MIN_Summary_Statistics.csv", row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
+write.csv(maxs, "MAX_Summary_Statistics.csv", row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+# ----- BOUND DATA IN A PLAUSIBLE RANGE ----- #
+
+bounded_file <- "INBOUNDS_AGGREGATE_Templates_11-16-17.csv"
+
+# fix PH range
+dataset$pH <- ifelse(dataset$pH >= 4 & dataset$pH <= 9,  dataset$pH, NA)
+
+# fix DO range
+dataset$DO_mg_L <- ifelse(dataset$DO_mg_L >=13,dataset$DO_mg_L, NA)
+
+# fix Conductivity range
+dataset$Conductivity <- ifelse(dataset$Conductivity >= 10, dataset$Conductivity, NA)
+
+# fix specific conductance range
+dataset$Spec_Cond <- ifelse(dataset$Spec_Cond >= 10, dataset$Spec_Cond, NA)
+
+# fix specific DOC range
+dataset$DOC <- ifelse(dataset$DOC >= 0.1 & dataset$DOC <= 150, dataset$DOC, NA)
+
+# fix specific TDN range
+dataset$TDN <- ifelse(dataset$TDN > 0.05 & dataset$TDN < 25, dataset$TDN, NA)
+
+# fix specific DON range
+dataset$DON <- ifelse(dataset$DON >= 0, dataset$DON, NA)
+
+# fix specific NO3 range
+dataset$NO3 <- ifelse(dataset$NO3 >= 0.01 & dataset$NO3 <= 17, dataset$NO3, NA)
+
+# fix specific NH4 range
+dataset$NH4 <- ifelse(dataset$NH4 >= 0.05, dataset$NH4, NA)
+
+# fix specific PO4P range
+dataset$PO4 <- ifelse(dataset$PO4 > 0.02 & dataset$PO4 < 2, dataset$PO4, NA)
+
+# fix specific SRP range
+dataset$SRP <- ifelse(dataset$SRP > 0.02 & dataset$SRP < 2, dataset$SRP, NA)
+
+# fix specific TDP range
+dataset$TDP <- ifelse(dataset$TDP > 0.02 & dataset$TDP < 10, dataset$TDP, NA)
+
+# fix specific DOP range
+dataset$DOP <- ifelse(dataset$DOP > 0.02 & dataset$DOP < 10, dataset$DOP, NA)
+
+# fix specific TPO4 range
+#dataset$TPO4 <- ifelse(dataset$TPO4 > 0.02 | dataset$TPO4 < 10, dataset$TPO4, NA)
+
+# fix specific POC range
+#dataset$POC <- ifelse(dataset$POC > 0.02 | dataset$POC < 10, dataset$POC, NA)
+
+# fix specific range
+for (c in c("Si", "Na", "Ca", "Mg", "SO4", "Cl")){ # "Fe", "Sr", "Al" 
+  dataset[, c] <- ifelse(dataset[, c] > 0.1, dataset[, c], NA)
+}
+
+# fix specific K range
+dataset$K <- ifelse(dataset$K > 0.1 & dataset$K < 30, dataset$K, NA)
+
+
+write.csv(dataset, bounded_file, row.names = FALSE, fileEncoding = "UTF-8", quote = TRUE)
+
+
+
+
+
+
+
+
+
